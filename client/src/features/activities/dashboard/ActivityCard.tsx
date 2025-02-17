ca8c6fda05cba3fdbff26394b2693ec90780617c
OscarVehmanen <oscarveh@gmail.com>
2025-02-17 21:08:45 +0100

    implemented react query, axios and crud

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,12 +1,21 @@
-import { Button, Box, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
+import {
+  Button,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
+  Typography,
+} from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
   activity: Activity;
   selectActivity: (id: string) => void;
-  deleteActivity: (id: string) => void;
 };
 
-export default function ActivityCard({ activity, selectActivity, deleteActivity }: Props) {
+export default function ActivityCard({ activity, selectActivity }: Props) {
+  const { deleteActivity } = useActivities();
   return (
     <Card sx={{ borderRadius: 3 }}>
       <CardContent>
@@ -19,11 +28,31 @@ export default function ActivityCard({ activity, selectActivity, deleteActivity 
           {activity.city} / {activity.venue}
         </Typography>
       </CardContent>
-      <CardActions sx={{display: "flex", justifyContent: "space-between", paddingBottom: 2}}>
+      <CardActions
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          paddingBottom: 2,
+        }}
+      >
         <Chip label={activity.category} variant="outlined"></Chip>
         <Box display={"flex"} gap={2}>
-          <Button onClick={() => selectActivity(activity.id)} size="medium" variant="contained">View</Button>
-          <Button onClick={() => deleteActivity(activity.id)} size="medium" variant="contained" color="error">Delete</Button>
+          <Button
+            onClick={() => selectActivity(activity.id)}
+            size="medium"
+            variant="contained"
+          >
+            View
+          </Button>
+          <Button
+            onClick={() => deleteActivity.mutate(activity.id)}
+            size="medium"
+            variant="contained"
+            color="error"
+            disabled={deleteActivity.isPending}
+          >
+            Delete
+          </Button>
         </Box>
       </CardActions>
     </Card>
--- client/src/features/activities/details/ActivityDetail.tsx
+++ client/src/features/activities/details/ActivityDetail.tsx
@@ -1,12 +1,27 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { useActivities } from "../../../lib/hooks/useActivities";
 
 type Props = {
-  activity: Activity;
+  selectedActivity: Activity;
   cancelSelectActivity: () => void;
   openForm: (id: string) => void;
-};  
+};
 
-export default function ActivityDetail({ activity, cancelSelectActivity, openForm }: Props) {
+export default function ActivityDetail({
+  selectedActivity,
+  cancelSelectActivity,
+  openForm,
+}: Props) {
+  const { activities } = useActivities();
+  const activity = activities?.find(x => x.id === selectedActivity.id);
+  if (!activity) return <Typography>Loading...</Typography>
   return (
     <Card sx={{ borderRadius: 3 }}>
       <CardMedia
@@ -15,12 +30,18 @@ export default function ActivityDetail({ activity, cancelSelectActivity, openFor
       />
       <CardContent>
         <Typography variant="h5">{activity.title}</Typography>
-        <Typography variant="subtitle1" fontWeight={"light"}>{activity.date}</Typography>
+        <Typography variant="subtitle1" fontWeight={"light"}>
+          {activity.date}
+        </Typography>
         <Typography variant="body1">{activity.description}</Typography>
       </CardContent>
       <CardActions>
-        <Button onClick={() => openForm(activity.id)} color="primary">Edit</Button>
-        <Button onClick={cancelSelectActivity} color="inherit">Cancel</Button>
+        <Button onClick={() => openForm(activity.id)} color="primary">
+          Edit
+        </Button>
+        <Button onClick={cancelSelectActivity} color="inherit">
+          Cancel
+        </Button>
       </CardActions>
     </Card>
   );
